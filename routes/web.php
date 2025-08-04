d7481d3818ba46ee810e7c2d090bdc84a9633f59
usernamedinesh <borod9200@gmail.com>
2025-08-04 15:31:11 +0530

    adding redirct to another page directly

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,25 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/welcome', function () {
     return view('welcome');
 });
 
-Route::get('/hello', function () {
-    return view('hello');
+Route::get('/login', function () {
+    return view('login');
 });
+
+// Route::get('/signup', function () {
+//     return view('signup');
+// });
+
+Route::view("/signup", "signup"); // sort form 
+
+//accepting an dynamice route and passing to page about
+Route::get('/about/{name}',function($name) {
+    return view('about', ['name'=>$name]);
+});
+
